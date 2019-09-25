004a9b7adf89db44d5954161ceeeaad4845c3b8d
beardog-ukr <you@example.com>
2019-09-25 22:38:02 +0300

    Added: one more example 'cmt_width' option

---
+++ examples/cmt_width/ex_80c.cpp
@@ -0,0 +1,14 @@
+#include <vector>
+#include <iostream>
+#include <algorithm>
+
+int main()
+{
+	int x = 10 + 10 + 12 + 13 + 14 + 15 + 16 + 17 + 18 + 19 + 20 + 21 + 22 + 23 + 24 + 25;
+
+	// The quick brown fox jumps over the lazy dog. The quick brown fox jumps over the lazy dog.
+	std::cout << "x is " << x << '\n';
+
+	return 0;
+}
+
