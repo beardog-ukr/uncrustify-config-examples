6b521e296215617ba9e472557013f44139e0a40c
beardog-ukr <you@example.com>
2019-10-08 16:26:00 +0300

    Added: example for 'nl_brace_else' option

---
+++ examples/nl_brace_else/add_01.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+int main()
+{
+	int x = 10;
+	int y = 20;
+
+	// if with else
+	if (x<y) {
+		std::cout << "Also x is less than " << y << '\n';
+	} else {
+		std::cout << "Also x is more (or equal) than " << y << '\n';
+	}
+
+}
+
