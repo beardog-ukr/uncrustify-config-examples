96360c072db9783e6348cbf6e23f24f1bda9ec8a
beardog-ukr <you@example.com>
2019-10-11 16:06:18 +0300

    Added: example for 'sp_angle_word' option

---
+++ examples/sp_angle_word/add_01.unc.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <memory> //shared_ptr
+#include <vector> // vector
+
+using namespace std;
+
+int main()
+{
+	// Create a vector containing integers
+	vector<int> v = {7, 5, 16, 8};
+
+	// Add two more integers to vector
+	v.push_back(25);
+	v.push_back(13);
+
+	shared_ptr<int> isp=make_shared<int>();
+	*isp = 5000;
+	v.push_back(*isp);
+
+	// Iterate and print values of vector
+	for(int n : v) {
+		cout << n << '\n';
+	}
+}
