47df0100af08e1766a33bf011b267ad2c9e8659e
beardog-ukr <you@example.com>
2019-11-28 18:16:56 +0200

    example for 'nl_before_block_comment' option

---
+++ examples/nl_before_block_comment/add_02.unc.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+
+/* Some comment
+ * (this is multiline comment)
+ */
+
+int gg = 10;
+int gl = 3; /*multiline
+             * starts on same line
+             */
+
+int main()
+{
+	int x = 10;
+	int y = 20;
+
+	/* Another variables
+	 * (this is multiline comment)
+	 */
+	int a = 20;
+	int b = 10;
+	int c = b;
+
+	if (x<y) {
+		c = a;
+	}
+
+	std::cout << "Finally c is " << c << '\n';
+}
+
