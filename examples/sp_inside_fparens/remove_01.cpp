e97efcc5a6a99b467730cea302b782d437262dfb
beardog-ukr <you@example.com>
2019-11-28 21:49:59 +0200

    added example for 'sp_inside_fparens' option

---
+++ examples/sp_inside_fparens/remove_01.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+int getTwo( )
+{
+	return 2;
+}
+
+int main()
+{
+	int a = 200;
+	int c = getTwo();
+
+	if (a<100) {
+		c = a;
+	}
+
+	std::cout << "Finally c is " << c << '\n';
+}
