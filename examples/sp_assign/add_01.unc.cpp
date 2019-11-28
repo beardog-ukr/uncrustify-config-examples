6b4284e71fa66e960deaed0a13637c3648706272
beardog-ukr <you@example.com>
2019-11-28 18:19:20 +0200

    example for 'sp_assign' option

---
+++ examples/sp_assign/add_01.unc.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <memory> //shared_ptr
+#include <vector> // vector
+
+using namespace std;
+
+int main()
+{
+	// Create a vector containing integers
+	vector<int>v = {7, 5, 16, 8};
+
+	shared_ptr<int>isp = make_shared<int>();
+	*isp = 5000;
+	v.push_back(*isp);
+
+	int x = 4, y = 5;
+	int z = x+y;
+
+	v.push_back(z);
+	v.push_back(13);
+
+
+	// Iterate and print values of vector
+	for(int n : v) {
+		cout << n << '\n';
+	}
+}
