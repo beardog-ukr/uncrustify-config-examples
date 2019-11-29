9a74ccfa8cfa8787f273a26df885f2e9131a6ecd
beardog-ukr <you@example.com>
2019-11-29 21:29:31 +0200

    added examples for 'mod_full_brace_for' option

---
+++ examples/mod_full_brace_for/remove_01.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+int main()
+{
+	int x = 10;
+	int sum = 0;
+
+	for (int i=0; i<x; i++) {
+		sum = sum+i;
+	}
+
+	for (int i=x-1; i>0; i--) {sum = sum-i;}
+
+	std::cout << "sum is " << sum << '\n';
+}
+
