d484c906b517e60a023df18d56211d00489cc1ca
beardog-ukr <you@example.com>
2019-11-29 13:26:28 +0200

    added example for 'nl_multi_line_cond' option

---
+++ examples/nl_multi_line_cond/ex_02.unc.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+int main()
+{
+	int x = 10;
+	int y = 20;
+
+	if ((x<5 )&&(y<10)) {
+		std::cout << "case #1" << '\n';
+	}
+
+	if ((x<5 )&&
+	    (y<10)) {
+		std::cout << "Also case #2" << y << '\n';
+	}
+}
