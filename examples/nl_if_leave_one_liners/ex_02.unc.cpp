7107656ecc1c563b63b744977579afece10c4ff6
beardog-ukr <you@example.com>
2019-11-29 12:40:08 +0200

    added example for 'nl_if_leave_one_liners' option

---
+++ examples/nl_if_leave_one_liners/ex_02.unc.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+int main()
+{
+	int a = 200;
+	int c = 10;
+
+	if (a<100) c = a;
+
+	if (c>5)
+	{
+		std::cout << "c is larger than 5" << '\n';
+	}
+
+	std::cout << "Finally c is " << c << '\n';
+}
